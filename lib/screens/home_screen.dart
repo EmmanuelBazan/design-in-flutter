cc4e2c23a59018d6991e28938807369dd3ff7853
EmmanuelBazan <emmanuelbazan99@gmail.com>
2023-07-19 12:37:09 -0600

    Card table implementation

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:design_in_flutter/widgets/background.dart';
+import 'package:design_in_flutter/widgets/card_table.dart';
 import 'package:design_in_flutter/widgets/custom_navigation.dart';
 import 'package:design_in_flutter/widgets/page_title.dart';
 import 'package:flutter/material.dart';
@@ -28,7 +29,7 @@ class _HomeBody extends StatelessWidget {
       child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [PageTitle()]),
+          children: const [PageTitle(), CardTable()]),
     );
   }
 }
---
+++ lib/widgets/card_table.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class CardTable extends StatelessWidget {
+  const CardTable({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Table(
+      children: const [
+        TableRow(children: [
+          _SingleCard(
+              color: Colors.blue, icon: Icons.pie_chart, title: 'General'),
+          _SingleCard(
+              color: Colors.pink,
+              icon: Icons.car_rental_rounded,
+              title: 'Transport')
+        ]),
+        TableRow(children: [
+          _SingleCard(
+              color: Colors.blue, icon: Icons.pie_chart, title: 'General'),
+          _SingleCard(
+              color: Colors.pink,
+              icon: Icons.car_rental_rounded,
+              title: 'Transport')
+        ]),
+        TableRow(children: [
+          _SingleCard(
+              color: Colors.blue, icon: Icons.pie_chart, title: 'General'),
+          _SingleCard(
+              color: Colors.pink,
+              icon: Icons.car_rental_rounded,
+              title: 'Transport')
+        ]),
+        TableRow(children: [
+          _SingleCard(
+              color: Colors.blue, icon: Icons.pie_chart, title: 'General'),
+          _SingleCard(
+              color: Colors.pink,
+              icon: Icons.car_rental_rounded,
+              title: 'Transport')
+        ]),
+        TableRow(children: [
+          _SingleCard(
+              color: Colors.blue, icon: Icons.pie_chart, title: 'General'),
+          _SingleCard(
+              color: Colors.pink,
+              icon: Icons.car_rental_rounded,
+              title: 'Transport')
+        ]),
+        TableRow(children: [
+          _SingleCard(
+              color: Colors.blue, icon: Icons.pie_chart, title: 'General'),
+          _SingleCard(
+              color: Colors.pink,
+              icon: Icons.car_rental_rounded,
+              title: 'Transport')
+        ]),
+      ],
+    );
+  }
+}
+
+class _SingleCard extends StatelessWidget {
+  final String title;
+  final IconData icon;
+  final Color color;
+
+  const _SingleCard({
+    super.key,
+    required this.title,
+    required this.icon,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(15),
+      height: 180,
+      decoration: BoxDecoration(
+        color: const Color.fromRGBO(62, 66, 107, 0.7),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        CircleAvatar(
+          backgroundColor: color,
+          radius: 30,
+          child: Icon(icon),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Text(
+          title,
+          style: TextStyle(color: color, fontSize: 18),
+        )
+      ]),
+    );
+  }
+}
