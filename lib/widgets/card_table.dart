c8f06dc387a122bb620afd7e6cce516270daf024
EmmanuelBazan <emmanuelbazan99@gmail.com>
2023-07-20 11:00:11 -0600

    Blur card implementation

--- lib/widgets/card_table.dart
+++ lib/widgets/card_table.dart
@@ -1,6 +1,6 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class CardTable extends StatelessWidget {
   const CardTable({super.key});
@@ -78,25 +78,34 @@ class _SingleCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(15),
-      height: 180,
-      decoration: BoxDecoration(
-        color: const Color.fromRGBO(62, 66, 107, 0.7),
+      child: ClipRRect(
         borderRadius: BorderRadius.circular(20),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+          child: Container(
+            height: 180,
+            decoration: BoxDecoration(
+              color: const Color.fromRGBO(62, 66, 107, 0.7),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              CircleAvatar(
+                backgroundColor: color,
+                radius: 30,
+                child: Icon(icon),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                title,
+                style: TextStyle(color: color, fontSize: 18),
+              )
+            ]),
+          ),
+        ),
       ),
-      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        CircleAvatar(
-          backgroundColor: color,
-          radius: 30,
-          child: Icon(icon),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        Text(
-          title,
-          style: TextStyle(color: color, fontSize: 18),
-        )
-      ]),
     );
   }
 }
